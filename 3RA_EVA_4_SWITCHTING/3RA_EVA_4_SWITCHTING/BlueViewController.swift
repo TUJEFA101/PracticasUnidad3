1f3512e1a3aa3e3a672a5e7770a726ce0d52e59c
TUJEFA101 <rafa_rock_96@hotmail.com>
2017-06-07 12:54:30 -0600

    Add files via upload

---
+++ 3RA_EVA_4_SWITCHTING/3RA_EVA_4_SWITCHTING/BlueViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BlueViewController.swift
+//  3RA_EVA_4_SWITCHTING
+//
+//  Created by LUIS FERNANDO on 04/05/17.
+//  Copyright © 2017 LUIS FERNANDO. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func blueAction(sender: AnyObject) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
